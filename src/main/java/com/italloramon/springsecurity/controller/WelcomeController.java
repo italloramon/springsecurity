4f9b6bb541ff2c8102bf8379f554c963e44dc8b1
italloramon <irvp@ic.ufal.br>
2023-04-02 17:04:42 -0300

    Upload files

---
+++ src/main/java/com/italloramon/springsecurity/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.italloramon.springsecurity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String sayWelcome() {
+        return "Welcome to spring application with spring security";
+    }
+}
