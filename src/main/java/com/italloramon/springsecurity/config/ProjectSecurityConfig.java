7a2d0d6ba5f2428c7c40f41a361278287d0a7fc4
italloramon <irvp@ic.ufal.br>
2023-04-10 07:18:08 -0300

    Configuring users using InMemoryDetailsManager

--- src/main/java/com/italloramon/springsecurity/config/ProjectSecurityConfig.java
+++ src/main/java/com/italloramon/springsecurity/config/ProjectSecurityConfig.java
@@ -1,8 +1,14 @@
 package com.italloramon.springsecurity.config;
 
+import org.springframework.cglib.proxy.NoOp;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -34,4 +40,42 @@ public class ProjectSecurityConfig {
                 .httpBasic();
 */
     }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+//        UserDetails admin = User.withDefaultPasswordEncoder()
+//                .username("admin")
+//                .password("123")
+//                .authorities("admin")
+//                .build();
+//
+//        UserDetails user = User.withDefaultPasswordEncoder()
+//                .username("user")
+//                .password("123")
+//                .authorities("read")
+//                .build();
+
+        /* Approach 2 where we use NoOpPasswordEncoder Bean while creating the user details */
+        UserDetails admin = User.withUsername("admin")
+                .password("123")
+                .authorities("admin")
+                .build();
+
+        UserDetails user = User.withUsername("user")
+                .password("123")
+                .authorities("read")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
 }
+
+
+
+
